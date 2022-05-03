fbe4b163867df90deeeaa64ac2b235d2063d1edf
Joshua Maddox <jpmaddox97@gmail.com>
2022-05-03 19:33:36 -0400

    initial api and python virtual environment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
-import { Container, Row, Col } from 'react-bootstrap';
 import Welcome from './components/Welcome';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // eslint-disable-next-line no-undef
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
@@ -39,18 +39,18 @@ const App = () => {
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
       <Container className="mt-4">
-        {images.length ? (<Row xs={1} md={2} lg={3}>
-          {images.map((image, i) => (
-            // eslint-disable-next-line react/jsx-key
-            <Col key={i} className="pb-3">
-              <ImageCard  image={image} deleteImage={handleDeleteImage}/>
-            </Col>
-          ))}
-        </Row>
+        {images.length ? (
+          <Row xs={1} md={2} lg={3}>
+            {images.map((image, i) => (
+              // eslint-disable-next-line react/jsx-key
+              <Col key={i} className="pb-3">
+                <ImageCard image={image} deleteImage={handleDeleteImage} />
+              </Col>
+            ))}
+          </Row>
         ) : (
-        <Welcome />
+          <Welcome />
         )}
-        
       </Container>
     </div>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,21 +4,21 @@ import { Navbar, Container } from 'react-bootstrap';
 import { ReactComponent as Logo } from '../images/logo.svg';
 
 const navbarStyle = {
-  backgroundColor: '#eeee'
-}
+  backgroundColor: '#eeee',
+};
 
 const Header = ({ title }) => {
-    return (
+  return (
     <Navbar style={navbarStyle} variant="light">
-        <Container>
-            <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }}/>
-        </Container>
+      <Container>
+        <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
+      </Container>
     </Navbar>
-      )
-}
+  );
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
