b72b1d70b2902f11df619ce8b500569da63a01ea
Joshua Maddox <jpmaddox97@gmail.com>
2022-05-04 13:51:50 -0400

    sample flask app

--- api/main.py
+++ api/main.py
@@ -1,1 +1,12 @@
-import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5050)
+
+#env FLASK_APP=main.py flask run
